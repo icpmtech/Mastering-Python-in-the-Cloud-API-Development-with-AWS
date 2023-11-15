0792ef643a7d0b96a00ed5733231437f5e8fed5c
Pedro Martins <mourao.martins@gmail.com>
2023-11-15 22:23:08 +0000

    add

---
+++ operators.py
@@ -0,0 +1,8 @@
+python
+# Basic arithmetic operators
+a = 10
+b = 3
+sum = a + b    # 13
+difference = a - b # 7
+product = a * b # 30
+quotient = a / b # 3.3333
--- variables_datatype.py
+++ variables_datatype.py
@@ -4,3 +4,48 @@ name = "Alice"  # String
 age = 30        # Integer
 height = 5.5    # Float
 is_student = True # Boolean
+
+python
+# This is a single-line comment in Python
+"""
+This is a multi-line comment
+or docstring used to describe more complex code
+"""
+
+python
+# Example of if-else statement
+age = 20
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
+
+python
+# Example of a for loop
+for i in range(5):
+    print(i)  # prints numbers 0 to 4
+# Example of a while loop
+count = 0
+while count < 5:
+    print(count)
+    count += 1  # same as count = count + 1
+
+
+python
+# Defining a simple function
+def greet(name):
+    return "Hello, " + name + "!"
+print(greet("Alice"))  # Output: Hello, Alice!
+
+
+python
+# Assuming we have a module named 'mymodule.py' with a function called 'greet'
+import mymodule
+mymodule.greet("Bob")  # Accessing the greet function from mymodule
+
+
+
+
+
+
+
