95f6126eb978fdc0f1ff8133dee17275393ae5d7
Pedro Martins <mourao.martins@gmail.com>
2023-11-17 17:26:26 +0000

    add

---
+++ Hands-on Coding/Basic Syntax Exercises/data_types.py
@@ -0,0 +1,11 @@
+
+# Data Types Example
+integer_var = 10
+float_var = 20.5
+string_var = "Hello Python"
+boolean_var = True
+
+print(integer_var)
+print(float_var)
+print(string_var)
+print(boolean_var)
