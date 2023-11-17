85a157adfdcf12e5e767f7b05d60c973d5027527
Pedro Martins <mourao.martins@gmail.com>
2023-11-17 17:45:30 +0000

    add samples

---
+++ Hands-on Coding/Basic Syntax Exercises/control_structures.py
@@ -0,0 +1,12 @@
+# Control Structures Example
+number = 15
+
+# If-else statement
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# Loop (for example, a simple for loop)
+for i in range(5):
+    print(i)
---
+++ Hands-on Coding/Function and Module Exercises/using_modules.py
@@ -0,0 +1,6 @@
+import math
+# Using various functions from the math module
+print(math.sqrt(16))  # Square root
+print(math.pow(2, 3)) # Power
+print(math.pi)        # PI constant
+print(math.log(10))   # Natural logarithm
